e08089e7f7b7486d97128e42b0b5c27d0b306498
Akshara Sadheesh <94154731+sharanair29@users.noreply.github.com>
2023-11-22 00:54:14 +0800

    add pyarrow compressed

---
+++ pyarrow/include/arrow/util/config.h
@@ -0,0 +1,61 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#define ARROW_VERSION_MAJOR 15
+#define ARROW_VERSION_MINOR 0
+#define ARROW_VERSION_PATCH 0
+#define ARROW_VERSION ((ARROW_VERSION_MAJOR * 1000) + ARROW_VERSION_MINOR) * 1000 + ARROW_VERSION_PATCH
+
+#define ARROW_VERSION_STRING "15.0.0-SNAPSHOT"
+
+#define ARROW_SO_VERSION "1500"
+#define ARROW_FULL_SO_VERSION "1500.0.0"
+
+#define ARROW_CXX_COMPILER_ID "GNU"
+#define ARROW_CXX_COMPILER_VERSION "11.4.0"
+#define ARROW_CXX_COMPILER_FLAGS " -fdiagnostics-color=always"
+
+#define ARROW_BUILD_TYPE "DEBUG"
+
+#define ARROW_GIT_ID "c5a1eb01631f770dd7b3a6ecf309022d34af4c1c"
+#define ARROW_GIT_DESCRIPTION "apache-arrow-15.0.0.dev-109-gc5a1eb016"
+
+#define ARROW_PACKAGE_KIND ""
+
+#define ARROW_COMPUTE
+#define ARROW_CSV
+/* #undef ARROW_CUDA */
+/* #undef ARROW_DATASET */
+#define ARROW_FILESYSTEM
+/* #undef ARROW_FLIGHT */
+/* #undef ARROW_FLIGHT_SQL */
+#define ARROW_IPC
+#define ARROW_JEMALLOC
+#define ARROW_JEMALLOC_VENDORED
+#define ARROW_JSON
+/* #undef ARROW_ORC */
+#define ARROW_PARQUET
+/* #undef ARROW_SUBSTRAIT */
+
+#define ARROW_ENABLE_THREADING
+/* #undef ARROW_GCS */
+/* #undef ARROW_S3 */
+#define ARROW_USE_NATIVE_INT128
+/* #undef ARROW_WITH_MUSL */
+/* #undef ARROW_WITH_OPENTELEMETRY */
+/* #undef ARROW_WITH_UCX */
+/* #undef PARQUET_REQUIRE_ENCRYPTION */
